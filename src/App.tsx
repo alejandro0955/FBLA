3dc4b08cfdc8c47c24a0234b8d52b7fbcc9c5bcc
alejandro0955 <alejandrosalza1223@gmail.com>
2024-10-15 13:52:57 -0400

    fixed gallery

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       </div>
       <InformationSlider />
       <BoardMembers />
-      {/* <Gallery /> */}
+      <Gallery />
       <JoinSection />
       <BottomSection />
     </>
