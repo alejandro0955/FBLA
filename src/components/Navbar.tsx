65881b14ee82ea19d12c28a0b6f75d322c7330af
alejandro0955 <33553049+alejandro0955@users.noreply.github.com>
2024-10-18 23:42:21 -0400

    fixed logos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,27 +5,27 @@ function Navbar() {
   return (
     <>
       <div className="navbar">
-        <img className="logo" src="/logo.png"></img>
-        <img src="/SmallLogo.png" className="small-logo"></img>
-        <ul className="navbar-options">
-          <li className="navbar-option">
-            <a href="#about-us">About Us</a>
-          </li>
-          <li className="navbar-option">
-            <a href="#our-board">Our Board</a>
-          </li>
-          <li className="navbar-option">
-            <a href="#gallery">Gallery</a>
-          </li>
-          <li className="navbar-option">
-            <a href="#join-fbla">Join FBLA</a>
-          </li>
-          <li className="navbar-option">
-            <a href="#socials">Socials</a>
-          </li>
-        </ul>
-        <HamburgerMenu />
-      </div>
+          <img className="logo" src="/logo.png"></img>
+          <img src="/SmallLogo.png" className="small-logo"></img>
+          <ul className="navbar-options">
+            <li className="navbar-option">
+              <a href="#about-us">About Us</a>
+            </li>
+            <li className="navbar-option">
+              <a href="#our-board">Our Board</a>
+            </li>
+            <li className="navbar-option">
+              <a href="#gallery">Gallery</a>
+            </li>
+            <li className="navbar-option">
+              <a href="#join-fbla">Join FBLA</a>
+            </li>
+            <li className="navbar-option">
+              <a href="#socials">Socials</a>
+            </li>
+          </ul>
+          <HamburgerMenu />
+        </div>
     </>
   );
 }
