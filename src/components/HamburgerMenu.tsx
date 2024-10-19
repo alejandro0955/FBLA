1974bbdd29ecb4f96f6578f754909bcae5150886
alejandro0955 <33553049+alejandro0955@users.noreply.github.com>
2024-10-19 14:37:30 -0400

    added navbar for mobile

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
 import "./HamburgerMenu.css";
 
-
 function HamburgerMenu() {
-  const [open,setOpen] = useState(false)
+  const [open, setOpen] = useState("closed");
   const setOpenClosed = () => {
-    if (!open) {
-      setOpen(true);
-      var l1id = document.getElementById("l1")
-      var l2id = document.getElementById("l2")
-      var l3id = document.getElementById("l3")
-      var nav = document.getElementsByClassName("navbar")
-      if (l1id && l2id && l3id ){
-        l1id.id = "l1-open"
-        l2id.id = "l2-open"
-        l3id.id = "l3-open"
-        if (nav) {
-          nav[0].id = "small-navbar"
+    var lines = document.getElementsByClassName("line");
+    var nav = document.getElementsByClassName("navbar");
+    if (open == "closed") {
+      setOpen("open");
+      console.log("open");
+      if (lines) {
+        for (let i = 0; i < lines.length; i++) {
+          lines[i].id = "l" + (i + 1).toString() + "-open";
         }
-
+      }
+      if (nav) {
+        nav[0].id = "small-navbar-open";
+      }
     } else {
-      setOpen(false)
-      console.log("Test")
+      setOpen("closed");
+      console.log("closed");
+      if (lines) {
+        for (let i = 0; i < lines.length; i++) {
+          lines[i].id = "";
+        }
+      }
+      if (nav) {
+        nav[0].id = "small-navbar-close";
+      }
     }
-  }
-}
+  };
   return (
     <>
       <div onClick={setOpenClosed} className="hmenu">
